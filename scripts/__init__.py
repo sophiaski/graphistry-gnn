3ab14181de940d11f0979191e2870d3637ca4df9
Sophia Skowronski <sski@pm.me>
2022-08-31 09:38:00 -0700

    Model dir is public, commenting out

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -6,7 +6,11 @@ try:
     with open(f"{__base__}/../.env", "r") as env_file:
         for line in env_file:
             line = line.strip()
-            if line.startswith("#") or (line.startswith("[") and line.endswith("]")) or not line:
+            if (
+                line.startswith("#")
+                or (line.startswith("[") and line.endswith("]"))
+                or not line
+            ):
                 continue
 
             var, val = line.split("=", maxsplit=1)
@@ -15,10 +19,10 @@ except OSError:
     pass
 
 DATA_DIR = os.getenv("DATA_DIR", f"{__base__}/../data")
-MODEL_DIR = os.getenv("MODEL_DIR", f"{__base__}/../models")
+# MODEL_DIR = os.getenv("MODEL_DIR", f"{__base__}/../models")
 
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 
-if not os.path.exists(MODEL_DIR):
-    os.mkdir(MODEL_DIR)
+# if not os.path.exists(MODEL_DIR):
+#     os.mkdir(MODEL_DIR)
